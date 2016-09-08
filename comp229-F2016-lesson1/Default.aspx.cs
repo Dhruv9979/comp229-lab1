26814c71e0473d1c4ea3c0ea4ddb634fd6aadc7f
300856248 <300856248@PR-A321-04>
2016-09-08 15:36:38 -0400

    Add project files.

---
+++ comp229-F2016-lesson1/Default.aspx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace comp229_F2016_lesson1
+{
+    public partial class _Default : Page
+    {
+        ///<summary>
+        ///this method executes every time the page loads
+        ///</summary>
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HelloLabel.Text = "Good Bye!!!!";
+        }
+    }
+}
